9c9d01bf39d3d8669c5660e69591853880ba6eb0
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2021-10-08 19:08:01 +0900

    Add files via upload

---
+++ ex.js
@@ -0,0 +1,16 @@
+const id = "pyosuyong";
+const password = "11223344";
+
+if(id === "pyosuyong") {
+    if(password === "11223344") {
+        console.log(`
+        아이디: ${id}
+        환영합니다.`);
+    }
+    else {
+        console.log("로그인 실패");
+    }
+}
+else {
+    console.log("로그인 실패");
+}
