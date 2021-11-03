5aaad67bcd3ca3fe6d675463cf32fcfaf0dee054
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2021-11-03 23:17:13 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,41 @@
+//메인웹툰 변경 함수들. 번호 클릭시 헤더의 스타일중 백그라운드이미지 요소 변경
+const sequence1 = () => {
+    const image1 = "./style/Mainwebtoon/2021최애캐 안녕, 잘 지내니.jpg"
+    document.getElementsByTagName("header")[0].style.backgroundImage = "url('" + image1 + "')";
+}
+
+const sequence2 = () => {
+    const image2 = "./style/Mainwebtoon/여신강림.jpg"
+    document.getElementsByTagName("header")[0].style.backgroundImage = "url('" + image2
+}
+
+const sequence3 = () => {
+    const image3 = "./style/Mainwebtoon/용사가돌아왔다.jpg"
+    document.getElementsByTagName("header")[0].style.backgroundImage = "url('" + image3 + "')";
+}
+
+const sequence4 = () => {
+    const image4 = "./style/Mainwebtoon/한림체육관.jpg"
+    document.getElementsByTagName("header")[0].style.backgroundImage = "url('" + image4 + "')";
+}
+
+const sequence5 = () => {
+    const image5 = "./style/Mainwebtoon/엽총소년.jpg"
+    document.getElementsByTagName("header")[0].style.backgroundImage = "url('" + image5 + "')";
+}
+
+const sequence6 = () => {
+    const image6 = "./style/Mainwebtoon/하루만네가되고싶어.jpg"
+    document.getElementsByTagName("header")[0].style.backgroundImage = "url('" + image6 + "')";
+}
+
+const sequence7 = () => {
+    const image7 = "./style/Mainwebtoon/사신소년.jpg"
+    document.getElementsByTagName("header")[0].style.backgroundImage = "url('" + image7 + "')";
+}
+
+const sequence8 = () => {
+    const image8 = "./style/Mainwebtoon/1을줄게.jpg"
+    document.getElementsByTagName("header")[0].style.backgroundImage = "url('" + image8 + "')";
+}
+//메인웹툰 변경함수들 끝
